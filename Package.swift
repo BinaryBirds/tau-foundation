18c68db97f137b3fd40aa0efed07c7467f1fd180
Tibor Bödecs <mail.tib@gmail.com>
2020-11-12 14:05:49 +0100

    Dep updates

--- Package.swift
+++ Package.swift
@@ -10,11 +10,13 @@ let package = Package(
         .library(name: "LeafFoundation", targets: ["LeafFoundation"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf", from: "4.0.0-tau"),
+        .package(url: "https://github.com/vapor/leaf", .exact("4.0.0-tau.1")),
+        .package(url: "https://github.com/vapor/leaf-kit", .exact("1.0.0-tau.1.1")),
     ],
     targets: [
         .target(name: "LeafFoundation", dependencies: [
             .product(name: "Leaf", package: "leaf"),
+            .product(name: "LeafKit", package: "leaf-kit"),
         ]),
         .testTarget(name: "LeafFoundationTests", dependencies: [
             .target(name: "LeafFoundation"),
